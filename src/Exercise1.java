2cbcc6543bc671118c13ab441c605c941a19b286
unknown <gabryelsoh@gmail.com>
2024-05-02 22:06:19 +0800

    commit exercise update

---
+++ src/Exercise1.java
@@ -0,0 +1,15 @@
+// https://www.w3resource.com/java-exercises/collection/java-collection-exercise-1.php
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        List<String> ls = new ArrayList<String>();
+        ls.add("Red");
+        ls.add("Green");
+        ls.add("Orange");
+        ls.add("White");
+        ls.add("Black");
+        System.out.println(ls);
+    }
+}
